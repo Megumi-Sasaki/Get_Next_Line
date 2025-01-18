8d23ef39f01fd4c3caa4f2c5b2616b03527d87bd
Megumi Sasaki <mesasaki@c1r2s2.42tokyo.jp>
2025-01-18 16:15:10 +0900

    add

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: mesasaki <mesasaki@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/12 14:57:20 by mesasaki          #+#    #+#             */
-/*   Updated: 2025/01/13 19:34:28 by mesasaki         ###   ########.fr       */
+/*   Updated: 2025/01/18 16:14:51 by mesasaki         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,15 +25,15 @@ char	*read_joint(int fd, char *handOver)
 	tmp = malloc((BUFFER_SIZE + 1) * sizeof(char));
 	if (!tmp || BUFFER_SIZE >= SIZE_MAX)
 		return (NULL);
-	bytesread = read(fd, tmp, BUFFER_SIZE); //読み込んだバイト数をリターン
-	if (bytesread <= 0)                     //読み込んだがEOF,何もなかった、もしくはエラーの場合
+	bytesread = read(fd, tmp, BUFFER_SIZE);
+	if (bytesread <= 0)
 	{
 		free(tmp);
 		return (NULL);
 	}
-	tmp[bytesread] = '\0'; //めっちゃ大事
+	tmp[bytesread] = '\0';
 	if (!handOver)
-		handOver = ft_strdup(""); // handOverがNULLなら空文字列で初期化？？？？？？？？？？？・
+		handOver = ft_strdup("");
 	new_handOver = ft_strjoin(handOver, tmp);
 	free(handOver);
 	free(tmp);
@@ -47,20 +47,20 @@ char	*get_line_from_handOver(char **handOver)
 	size_t	i;
 
 	i = 0;
-	if (!*handOver) //この処理いるか要確認
+	if (!*handOver)
 		return (NULL);
 	while ((*handOver)[i] && (*handOver)[i] != '\n')
 		i++;
 	if ((*handOver)[i] == '\n')
 	{
-		line = malloc(i + 2); // 改行含む + '\0'
+		line = malloc(i + 2);
 		if (!line)
 			return (NULL);
 		ft_memcpy(line, *handOver, i + 1);
 		line[i + 1] = '\0';
-		tmp = ft_strdup(*handOver + i + 1); // 残りを tmp にコピー（改行の後ろから）
-		free(*handOver);                    //ここでhandOver解放
-		*handOver = tmp;                    //改行のあとの残りの文字たちが入った。
+		tmp = ft_strdup(*handOver + i + 1);
+		free(*handOver);
+		*handOver = tmp;
 	}
 	return (line);
 }
@@ -68,23 +68,19 @@ char	*get_line_from_handOver(char **handOver)
 char	*get_next_line(int fd)
 {
 	char		*line;
-	char		*prev;
 	static char	*handOver;
+	char		*prev;
 
-	if (fd < 0 || BUFFER_SIZE <= 0 || BUFFER_SIZE > SSIZE_MAX)
+	if (fd < 0 || BUFFER_SIZE <= 0 || BUFFER_SIZE >= SIZE_MAX)
 		return (NULL);
 	while (!handOver || !ft_strchr(handOver, '\n'))
 	{
-		prev = handOver;                     //以前の保持
-		handOver = read_joint(fd, handOver); //新しいデータを読み込んで上書き
-		if (!handOver)                       //読み込んだ結果、EOFの場合
+		prev = handOver;
+		handOver = read_joint(fd, handOver);
+		if (!handOver)
 			return (prev);
-		if (prev == handOver && *prev != '\0') //すべて読んだが、新しいものはなかった// 残っている内容を返却
-			return (line = ft_strdup(prev), free(prev), handOver = NULL, line);
-		if (prev == handOver)
-			return (NULL);
 	}
-	line = get_line_from_handOver(&handOver); //実行後handOverはfreeされる
+	line = get_line_from_handOver(&handOver);
 	return (line);
 }
 
